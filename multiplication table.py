8becb27030cf551f3a0762bc28f038ca9b058bb3
lisa das <126893470+lisadascse72@users.noreply.github.com>
2024-07-09 14:34:23 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,8 @@
+def print_multiplication_table_of_2():
+    number = 2
+    for i in range(1, 11):
+        product = number * i
+        print(f"{number} x {i} = {product}")
+
+# Example usage
+print_multiplication_table_of_2()
